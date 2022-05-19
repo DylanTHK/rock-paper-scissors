776a6456d5b03e5ccb47f48f0b091a19dcbc46d5
DylanTHK <dylantanhk@gmail.com>
2022-05-19 15:43:25 +0800

    added playRound and game functions to allow for whole gameplay Note: text in playround sent directly to console.log instead of returning to function

--- game-logic.js
+++ game-logic.js
@@ -1,6 +1,6 @@
 
 // function to obtain user's input - playerSelection
-function playerSelection() {
+function playerPlay() {
     let player = prompt("Please choose Rock, Paper or Scissors").toLowerCase();
     // make input case insensitive
     let firstLetter = player[0];
@@ -11,7 +11,7 @@ function playerSelection() {
 }
 
 // function to determine the computer's choice - computerPlay, return computerSelection
-function computerSelection() {
+function computerPlay() {
     const options = ["Rock", "Paper", "Scissors"];
     let computer = Math.floor(Math.random() * 3);
     return options[computer];
@@ -25,18 +25,43 @@ function playRound(player, computer) {
     }
     // condition for win
     else if ((player === "Rock" && computer === "Scissors") || (player === "Paper" && computer === "Rock") || (player === "Scissors" && computer === "Paper")) {
-        return `${player} beats ${computer}, Player wins this round!`;
+        console.log(`${player} beats ${computer}, You WIN!`);
+        return "player";
     }
 
     // condition for lose
     else {
-        return `${computer} beats ${player}, Computer wins this round!`;
+        console.log(`${computer} beats ${player}, You LOSE!`);
+        return "computer";
     }
 }
 
 // function to manage state of game - game()
-//play 5 rounds, keep score, report a winner
+function game() {
+    //play 5 rounds, keep score, report a winner
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameRounds = 5; // determines number of rounds to play
 
-let computer = computerSelection();
-let player = playerSelection();
-let outcome = playRound(player, computer);
+    // loop to keep track of 5 rounds
+    for (let i = 0; i < gameRounds; i++) {
+        const computerSelection = computerPlay(); // new computer input each round
+        const playerSelection = playerPlay(); // new player input each round
+        let outCome = playRound(playerSelection, computerSelection); // to output the verdict of each round
+        if (outCome === "player") {
+            playerScore += 1;
+        } else {
+            computerScore += 1;
+        }
+    }
+
+    //condition to determine winner
+    if (playerScore > computerScore) {
+        console.log("Player WINS!");
+    } else {
+        console.log("Computer WINS!");
+    }
+}
+
+// function call to initiate game
+game()
