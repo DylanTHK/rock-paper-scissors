3907ac43a8e338a7803370cbfb9d61d48d315c84
DylanTHK <dylantanhk@gmail.com>
2022-05-19 10:59:20 +0800

    Created separate js file game-logic.js and added comments to js file for structure of code

---
+++ game-logic.js
@@ -0,0 +1,16 @@
+
+// function to obtain user's input - playerSelection
+
+// make input case insensitive
+
+
+
+// function to determine the computer's choice - computerPlay, return computerSelection
+
+// function to determine the outcome of the play -playRound
+
+
+// function to keep track of score - game()
+//play 5 rounds, keep score, report a winner
+
+
