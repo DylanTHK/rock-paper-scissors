6c5d7cfe273574f4e1417c994bcaa8e3234151da
DylanTHK <dylantanhk@gmail.com>
2022-05-19 11:41:29 +0800

    Added playerSelection function - takes input word to generate output with first letter capitalised
    Added computerSelection function - generates computer's option Rock, Paper or Scissors

--- game-logic.js
+++ game-logic.js
@@ -1,16 +1,38 @@
 
 // function to obtain user's input - playerSelection
-
-// make input case insensitive
-
-
+function playerSelection() {
+    let player = prompt("Please choose Rock, Paper or Scissors").toLowerCase();
+    // make input case insensitive
+    let firstLetter = player[0];
+    let capLetter = firstLetter.toUpperCase();
+    let restOfWord = player.slice(1);
+    let chosenOption = capLetter.concat(restOfWord);
+    return chosenOption;
+}
 
 // function to determine the computer's choice - computerPlay, return computerSelection
+function computerSelection() {
+    const options = ["Rock", "Paper", "Scissors"];
+    let computer = Math.floor(Math.random() * 3);
+    return options[computer];
+}
 
-// function to determine the outcome of the play -playRound
+// function to determine the outcome of the play - playRound
+function playRound(player, computer) {
+    // condition for tie
+    if (player === computer) {
+        return 
+    }
+    // condition for win
+    if (player === "Rock" && computer ==="Scissors") {
+        
+    }
 
+    // condition for lose
+}
 
-// function to keep track of score - game()
+// function to manage state of game - game()
 //play 5 rounds, keep score, report a winner
 
-
+let computer = computerSelection();
+let player = playerSelection();
